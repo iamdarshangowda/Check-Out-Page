a0ec2e655f0adbf5119b2e3d9ae003317772a604
Darshan Gowda <iamdarshangowda@gmail.com>
2022-04-26 20:18:35 +0530

    css updated

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* const bagDecrease = document.querySelector(".decrement-bag");
+const bagIncrease = document.querySelector(".increment-bag");
+const totalValue = 148.98;
+const total = documents.querySelector(".total");
+//const bagQty = document.querySelector(".qantity");
+const bagQtyValue = document.querySelector(".qantity").value;
+
+const newTotal = bagIncrease.addEventListener("click", myFunction);
+
+function myFunction() {
+  bagQtyValue++;
+  return (totalValue = totalValue + 54.99);
+}
+
+total.textContent = `$ ${newTotal}`;   */
